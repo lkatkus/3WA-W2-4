268623d84a286a9d63cdacfe98d5d177bcec8a85
lkatkus <laimonas@katkus.eu>
2018-01-05 13:16:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// console.log("console hello");
+//
+// alert("hello world");
+
+var var1;
+var var2;
+var var3;
+
+var a = "hello";
+var b = "world";
+var c = a + " " +  b;
+
+function newLog(){
+    a = prompt("new console log?");
+    console.log("naujas console.log - " + a);
+}
+
+function veiksmas(){
+    var1 = prompt("pirmas demuo");
+    var2 = prompt("antras demuo");
+    var3 = Number(Number(var1) + Number(var2));
+    console.log("Sum is equal to - " + var3);
+}
+
+// console.log("variable 1 - " + var1);
+// console.log("variable 2 - " + var2);
+// console.log("variable 3 - " + var3);
+
+// var a = 1/3;
+// console.log(a);
+// console.log(a*3);
+
+// SVARBU - pabaigoje lieka 3
+console.log(2.2*0.1);
+
+console.log(c);
+
+// BACKSLASHAS "\" - ESCAPE CHARACTER
+console.log("hello my \"friend\" and \\\\\\");
+
+
+/* TWO WAYS OF COMMENTS */
+
+// COMMENTS OUT THE WHOLE LINE
