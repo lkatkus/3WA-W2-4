35a0b46f5ec43fe84f282517ce697f9fe184c806
lkatkus <laimonas@katkus.eu>
2018-01-05 16:34:18 +0200

    Last commit of the day.

--- script.js
+++ script.js
@@ -56,6 +56,105 @@ function test2(){
     console.log("palanga " + x);
 }
 
+function test3(){
+    var x = 0;
+    var z = 0;
+    while (x++ < 10){
+        z = x + z;
+        // z += x++;
+        console.log(z);
+    }
+}
+
+function test4(){
+    var x = 0;
+    for(var i = 0; i < 10; i++){
+        x = x + i;
+        console.log(x);
+    }
+}
+
+function doWhile(){
+    x = 0;
+    y = 0;
+    do{
+        x = x + y;
+        y++;
+        console.log(x);
+    }
+    while(y < 10);
+}
+
+function test5(){
+    var s = 0;
+    var i = 0;
+    while(true){
+        s=i++;
+        console.log(s);
+        if(i>20){
+            break;
+        }
+    }
+    console.log("PABAIGA");
+}
+
+function test6(){
+    var s = 0;
+    for(var i = 0; i <= 10; i++){
+        console.log("cycle i " + i);
+        console.log("cycle s " + s);
+        s+=i;
+        console.log("cycle sum " + s);
+    }
+    console.log("END s " + s);
+    console.log("END i " + i);
+}
+
+function test7(){
+    var s = 0;
+    var i = 0;
+    for(;i<=10;){ /*for loop variables can be set ousite. ";" are needed */
+        s+=i++;
+        console.log(s);
+    }
+    console.log("END");
+}
+
+function test8(){
+    var s = 0;
+    for(var i = 0; i < 100; i += 3){
+        // console.log("cycle s - " + s);
+        // console.log("cycle i - " + i);
+        s += i;
+        console.log("cycle sum - " + s);
+    }
+    console.log("CYCLE END");
+}
+
+function test9(m){
+    var s = 0;
+    var n = 1;
+    for(var i = 0; i < 100; i += m){
+        console.log("=== CYCLE " + n + " START ===");
+        console.log("cycle s - " + s);
+        console.log("cycle i - " + i);
+        s += i;
+        console.log("=== CYCLE " + n + " END ===");
+        console.log("");
+        n++;
+    }
+    console.log("=========");
+    console.log("CYCLE END");
+    console.log("=========");
+}
+
+function test10() {
+    for(var i = 0, j=10; i<=j ; i++, j--){ /* IMPORTANT!!! FIRST - EVALUATION, SECOND - BLOCK, THIRD - ADDITION / ETC.!!! multiple variables can be declared in for loops */
+        console.log(i,j);
+    }
+    console.log("END");
+}
+
 // console.log("variable 1 - " + var1);
 // console.log("variable 2 - " + var2);
 // console.log("variable 3 - " + var3);
